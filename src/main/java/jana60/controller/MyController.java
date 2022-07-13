f387cf7339def029348837235f9a69ad22e21143
LuigiZannella <zannellaluigi@outlook.com>
2022-07-13 14:43:15 +0200

    Esercizio finito

---
+++ src/main/java/jana60/controller/MyController.java
@@ -0,0 +1,28 @@
+package jana60.controller;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class MyController {
+
+	@GetMapping("/home")
+	public String home() {
+		return "home";
+	}
+
+	@GetMapping("/ora")
+	public String orario(Model model) {
+		LocalDateTime now = LocalDateTime.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+		model.addAttribute("orario", now.format(formatter));
+		return "ora";
+	}
+
+}
